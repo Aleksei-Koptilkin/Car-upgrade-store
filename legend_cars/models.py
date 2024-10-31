d8b57ca1c12a5299fc5bed2d0e6f722a2e75c313
Koptilkin Aleksey <alexey199419941994@mail.ru>
2024-10-31 23:12:54 +0400

    Добавил первую модель

--- legend_cars/models.py
+++ legend_cars/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    link = models.CharField(max_length=50, verbose_name="Ссылка")
+    car_brand = models.CharField(max_length=50, verbose_name="Марка")
+    model = models.CharField(max_length=50, verbose_name="Модель")
+    version = models.CharField(max_length=50, verbose_name="Версия",null=True)
+    year_start_production = models.IntegerField(verbose_name="Год начала производства")
+    year_end_production = models.IntegerField(verbose_name="Год окончания производства")
+    engine = models.CharField(max_length=50, verbose_name="Двигатель")
+    power_hp = models.IntegerField(verbose_name="Мощность в л.с.")
+
