0947e8dc3536a025897e86a082ad4e3254dea921
Koptilkin Aleksey <alexey199419941994@mail.ru>
2024-12-09 22:25:52 +0400

    Настроил админку приложения legend_cars

--- legend_cars/admin.py
+++ legend_cars/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from . models import *
 
-admin.site.register(Car)
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['car_brand', 'model', 'body_index', 'version', 'year_start_production', 'year_end_production',
+                    'engine', 'engine_displacement', 'power_hp']
+    ordering = ['car_brand']
+    search_fields = ['car_brand', 'model', 'body_index', 'version']
+
